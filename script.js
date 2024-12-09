21d6771179da0554486519b688f9383232e20571
George <georgekutsia@gmail.com>
2024-12-09 11:13:15 +0000

    navbar y titulo giratorio y cambios de tamaño en texto

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const buttonSolteros$$ = document.querySelector("button:nth-child(1)");
+  const buttonSolteros$$ = document.querySelector("button:nth-child(2)");
+  const buttonParejas$$ = document.querySelector("button:nth-child(3)");
+  const buttonFormula$$ = document.querySelector("button:nth-child(4)");
   const solterosBox$$ = document.getElementById("solteros-box");
+  const historiasFelicesBox$$ = document.getElementById("historias-felices-box");
+  const formulariosBox$$ = document.getElementById("formularios-box");
 
   buttonSolteros$$.addEventListener("click", () => {
-      if (solterosBox$$.style.display === "none" || solterosBox$$.style.display === "") {
           solterosBox$$.style.display = "block"; 
-      } else {
+          historiasFelicesBox$$.style.display = "none";
+          formulariosBox$$.style.display = "none"; 
+  });
+  buttonParejas$$.addEventListener("click", () => {
+          historiasFelicesBox$$.style.display = "block"; 
+          formulariosBox$$.style.display = "none"; 
           solterosBox$$.style.display = "none"; 
-      }
+  });
+  buttonFormula$$.addEventListener("click", () => {
+          formulariosBox$$.style.display = "block"; 
+          historiasFelicesBox$$.style.display = "none"; 
+          solterosBox$$.style.display = "none"; 
+
   });
 });
